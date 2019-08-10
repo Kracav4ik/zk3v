7e8b2f1d4152f110215ba052f236dffc445ba2c8
Kracav4ik <dikama2013@yandex.ru>
2019-08-10 17:35:13 +0300

    Initial commit

---
+++ MainWindow.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import QMainWindow
+import ui.ui_MainWindow as ui_MainWindow
+
+
+class MainWindow(QMainWindow, ui_MainWindow.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
